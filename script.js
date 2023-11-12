896168875e2b5d82b3c13275614282b5b1e7b2c1
Reggie Cheston <reggiecheston@AdminisatorsMBP.lan>
2023-11-12 12:38:23 -0500

    adds README, resolves undefined errors in js, finalizes validation and error message functionality in js, modifies error message styling to not interfere with js functionality, modifies html structure for more control over elements

--- script.js
+++ script.js
@@ -22,16 +22,18 @@ class MyTotalIncome extends Budget {
     super(total);
   }
   display(val1, val2, val3, val4) {
-    val1 = parseFloat(val2.value);
-    this.total += val1;
-    let incomeItem = document.createElement("li");
-    incomeItem.textContent = `${val4}: $${val1.toFixed(2)}`;
-    document.querySelector(".income__items").appendChild(incomeItem);
-    val3 = this.total;
-    document.getElementById(
-      "income__input--total"
-    ).textContent = `$${val3.toFixed(2)}`;
-    console.log(`Total income: ${val3.toFixed(2)}`);
+    if (val2.value != "") {
+      val1 = parseFloat(val2.value);
+      this.total += val1;
+      let incomeItem = document.createElement("li");
+      incomeItem.textContent = `${val4}: $${val1.toFixed(2)}`;
+      document.querySelector(".income__items").appendChild(incomeItem);
+      val3 = this.total;
+      document.getElementById(
+        "income__input--total"
+      ).textContent = `$${val3.toFixed(2)}`;
+      console.log(`Total income: ${val3.toFixed(2)}`);
+    }
     return;
   }
 }
@@ -42,16 +44,18 @@ class MyTotalExpenses extends Budget {
     super(total);
   }
   display(val1, val2, val3, val4) {
-    val1 = parseFloat(val2.value);
-    this.total -= val1;
-    let expensesItem = document.createElement("li");
-    expensesItem.textContent = `${val4}: $-${val1.toFixed(2)}`;
-    document.querySelector(".expenses__items").appendChild(expensesItem);
-    val3 = this.total;
-    document.getElementById(
-      "expenses__input--total"
-    ).textContent = `$${val3.toFixed(2)}`;
-    console.log(`Total expenses: ${val3.toFixed(2)}`);
+    if (val2.value != "") {
+      val1 = parseFloat(val2.value);
+      this.total -= val1;
+      let expensesItem = document.createElement("li");
+      expensesItem.textContent = `${val4}: $-${val1.toFixed(2)}`;
+      document.querySelector(".expenses__items").appendChild(expensesItem);
+      val3 = this.total;
+      document.getElementById(
+        "expenses__input--total"
+      ).textContent = `$${val3.toFixed(2)}`;
+      console.log(`Total expenses: ${val3.toFixed(2)}`);
+    }
     return;
   }
 }
@@ -83,17 +87,18 @@ incomeBtn.addEventListener("click", function () {
     "income__description"
   ).value;
 
-  //   Validate Description
+  // Validate Description
   if (incomeDescription === "") {
-    errorMessage(incomeDescription, "Must include a valid item name");
+    errorMessage("income__description", "Must include a valid ID");
   } else {
-    setSuccess(incomeDescription);
+    setSuccess("income__description");
   }
+
   // Validate Amount
   if (!isValidAmount(incomeInput)) {
-    errorMessage(income, "Must include a valid dollar amount");
+    errorMessage("income__input", "Must include a valid dollar amount");
   } else {
-    setSuccess(income);
+    setSuccess("income__input");
   }
 
   myIncome.display(incomeInput, income, totalIncome, incomeDescription);
@@ -107,17 +112,18 @@ expensesBtn.addEventListener("click", function () {
     "expenses__description"
   ).value;
 
-  //   Validate Description
+  // Validate Description
   if (expensesDescription === "") {
-    errorMessage(expensesDescription, "Must include a valid item name");
+    errorMessage("expenses__description", "Must include a valid ID");
   } else {
-    setSuccess(expensesDescription);
+    setSuccess("expenses__description");
   }
+
   // Validate Amount
   if (!isValidAmount(expensesInput)) {
-    errorMessage(expenses, "Must include a valid dollar amount");
+    errorMessage("expenses__input", "Must include a valid dollar amount");
   } else {
-    setSuccess(expenses);
+    setSuccess("expenses__input");
   }
 
   myExpenses.display(
@@ -130,22 +136,24 @@ expensesBtn.addEventListener("click", function () {
   myBudget.display();
 });
 
-const errorMessage = (element, message) => {
-  const inputControl = element.parentElement;
-  //   undefined?
-  const errorDisplay = document.querySelector(".error");
+const errorMessage = (elementId, message) => {
+  const inputControl = document.getElementById(elementId).parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
 
-  errorDisplay.textContent = message;
-  inputControl.classList.add("error");
+  if (errorDisplay) {
+    errorDisplay.textContent = message;
+    inputControl.classList.add("error");
+  }
 };
 
-const setSuccess = (element) => {
-  const inputControl = element.parentElement;
-  //   undefined?
-  const errorDisplay = document.querySelector(".error");
+const setSuccess = (elementId) => {
+  const inputControl = document.getElementById(elementId).parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
 
-  errorDisplay.textContent = "";
-  inputControl.classList.remove("error");
+  if (errorDisplay) {
+    errorDisplay.textContent = "";
+    inputControl.classList.remove("error");
+  }
 };
 
 // regex to define input validity
